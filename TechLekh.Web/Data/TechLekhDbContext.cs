2cdc21beb164d219d053a8cffea2215153ad0679
Mausin Khan <mausin12@gmail.com>
2026-01-22 17:28:57 +0530

    Add application DbContext - TechLekhDbContext

---
+++ TechLekh.Web/Data/TechLekhDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechLekh.Web.Models.Domain;
+
+namespace TechLekh.Web.Data
+{
+    public class TechLekhDbContext : DbContext
+    {
+        public TechLekhDbContext(DbContextOptions<TechLekhDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
