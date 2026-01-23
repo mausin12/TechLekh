664757c827780210c7f679c01325b47e623a8bfa
Mausin Khan <mausin12@gmail.com>
2026-01-23 12:14:26 +0530

    feat(tag): add AdminTagsController, Add.cshtml view, and TagAddViewModel

---
+++ TechLekh.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechLekh.Web.Models.Domain;
+using TechLekh.Web.Models.ViewModels;
+
+namespace TechLekh.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public async Task<IActionResult> Add(TagAddViewModel viewModel)
+        {
+            return View();
+        }
+    }
+}
---
+++ TechLekh.Web/Models/ViewModels/TagAddViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechLekh.Web.Models.ViewModels
+{
+    public class TagAddViewModel
+    {
+        public string Name { get; set; }
+
+        public string DisplayName { get; set; }
+    }
+}
