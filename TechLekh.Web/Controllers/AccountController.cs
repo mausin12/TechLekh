2e2174ff549536b4dd11fa1337d4e26abc13f92b
Mausin Khan <mausin12@gmail.com>
2026-01-25 20:31:25 +0530

    feat(auth): implement user registration

---
+++ TechLekh.Web/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TechLekh.Web.Models.ViewModels;
+
+namespace TechLekh.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager)
+        {
+            this._userManager = userManager;
+            this._signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var identityUser = new IdentityUser
+                {
+                    UserName = viewModel.Username,
+                    Email = viewModel.Email
+                };
+                var identityResult = await _userManager.CreateAsync(identityUser, viewModel.Password);
+                if (identityResult.Succeeded)
+                {
+                    var roleIdentityResult = await _userManager.AddToRoleAsync(identityUser, "User");
+                    if (roleIdentityResult.Succeeded)
+                    {
+                        //Show success Notification
+                        return RedirectToAction("Register");
+                    }
+                }
+            }
+
+            return View();
+        }
+    }
+}
---
+++ TechLekh.Web/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechLekh.Web.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+    }
+}
