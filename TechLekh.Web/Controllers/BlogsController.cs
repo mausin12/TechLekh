9db108cce1a17af982e94f55d7bcba274370a9de
Mausin Khan <mausin12@gmail.com>
2026-01-25 15:42:24 +0530

    feat(blog): show individual blog post

---
+++ TechLekh.Web/Controllers/BlogsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TechLekh.Web.Models.ViewModels;
+using TechLekh.Web.Repositories;
+
+namespace TechLekh.Web.Controllers
+{
+    public class BlogsController : Controller
+    {
+        private readonly IBlogPostRepository _blogPostRepository;
+
+        public BlogsController(IBlogPostRepository blogPostRepository)
+        {
+            this._blogPostRepository = blogPostRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(string urlHandle)
+        {
+            var blogPost = await _blogPostRepository.GetByUrlHandle(urlHandle);
+            var viewModel = new BlogDetaisViewModel();
+            if (blogPost != null)
+            {
+                viewModel = new BlogDetaisViewModel
+                {
+                    Id = blogPost.Id,
+                    Heading = blogPost.Heading,
+                    PageTitle = blogPost.PageTitle,
+                    Content = blogPost.Content,
+                    ShortDescription = blogPost.ShortDescription,
+                    FeaturedImageUrl = blogPost.FeaturedImageUrl,
+                    UrlHandle = blogPost.UrlHandle,
+                    PublishedDate = blogPost.PublishedDate,
+                    Author = blogPost.Author,
+                    Visible = blogPost.Visible,
+                    Tags = blogPost.Tags,
+                };
+            }
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ TechLekh.Web/Models/ViewModels/BlogDetaisViewModel.cs
@@ -0,0 +1,20 @@
+﻿using TechLekh.Web.Models.Domain;
+
+namespace TechLekh.Web.Models.ViewModels
+{
+    public class BlogDetaisViewModel
+    {
+        public Guid Id { get; set; }
+        public string Heading { get; set; }
+        public string PageTitle { get; set; }
+        public string Content { get; set; }
+        public string ShortDescription { get; set; }
+        public string FeaturedImageUrl { get; set; }
+        public string UrlHandle { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Author { get; set; }
+        public bool Visible { get; set; }
+
+        public ICollection<Tag> Tags { get; set; }
+    }
+}
