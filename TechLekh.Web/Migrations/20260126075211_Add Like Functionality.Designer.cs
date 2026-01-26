80664262f0e3391c4a39ed9301688b7192b41b9f
Mausin Khan <mausin12@gmail.com>
2026-01-26 13:27:10 +0530

    chore(db): add migration for BlogPostLike model

--- TechLekh.Web/Migrations/20260126075211_Add Like Functionality.Designer.cs
+++ TechLekh.Web/Migrations/20260126075211_Add Like Functionality.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TechLekh.Web.Data;
 
@@ -11,9 +12,11 @@ using TechLekh.Web.Data;
 namespace TechLekh.Web.Migrations
 {
     [DbContext(typeof(TechLekhDbContext))]
-    partial class TechLekhDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260126075211_Add Like Functionality")]
+    partial class AddLikeFunctionality
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
