e828f365e55c834e1924fe9759cdb5bddc851554
Mausin Khan <mausin12@gmail.com>
2026-01-26 13:18:23 +0530

    feat(like): add BlogPostLike model

--- TechLekh.Web/Data/TechLekhDbContext.cs
+++ TechLekh.Web/Data/TechLekhDbContext.cs
@@ -11,5 +11,6 @@ namespace TechLekh.Web.Data
 
         public DbSet<BlogPost> BlogPosts { get; set; }
         public DbSet<Tag> Tags { get; set; }
+        public DbSet<BlogPostLike> BlogPostLikes { get; set; }
     }
 }
