cc6481000489ae93d88ecdc93cbf502574dfae25
Mausin Khan <mausin12@gmail.com>
2026-01-27 12:32:44 +0530

    feat(users): implement user create

--- TechLekh.Web/Controllers/AdminUsersController.cs
+++ TechLekh.Web/Controllers/AdminUsersController.cs
@@ -10,13 +10,16 @@ namespace TechLekh.Web.Controllers
     public class AdminUsersController : Controller
     {
         private readonly IUserRepository _userRepository;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public AdminUsersController(IUserRepository userRepository)
+        public AdminUsersController(IUserRepository userRepository,
+            UserManager<IdentityUser> userManager)
         {
             this._userRepository = userRepository;
+            this._userManager = userManager;
         }
 
-            [HttpGet]
+        [HttpGet]
         public async Task<IActionResult> List()
         {
             var users = (await _userRepository.GetAll())
@@ -31,6 +34,33 @@ namespace TechLekh.Web.Controllers
             {
                 Users = users
             });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> List(UserViewModel viewModel)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = viewModel.Username,
+                Email = viewModel.Email
+            };
+            var identityResult = await _userManager.CreateAsync(identityUser, viewModel.Password);
+
+            if (identityResult is not null && identityResult.Succeeded)
+            {
+                var roles = new List<string> { "User" };
+                if (viewModel.IsInAdminrole)
+                {
+                    roles.Add("Admin");
+                }
+                identityResult = await _userManager.AddToRolesAsync(identityUser, roles);
+
+                if (identityResult is not null && identityResult.Succeeded)
+                {
+                    return RedirectToAction("List", "AdminUsers");
+                }
+            }
+            return View();
         }
     }
 }
--- TechLekh.Web/Models/ViewModels/UserViewModel.cs
+++ TechLekh.Web/Models/ViewModels/UserViewModel.cs
@@ -4,5 +4,9 @@
     {
         public IEnumerable<UserListItemViewModel> Users { get; set; }
 
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsInAdminrole { get; set; }
     }
 }
