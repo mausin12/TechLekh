767e00c74c7c0897c48fc5996f225770c927d415
Mausin Khan <mausin12@gmail.com>
2026-02-23 16:16:45 +0530

    chore(dep): reorganize project dependecies to follow CLEAN arch

--- TechLekh.Core/Domain/Tag.cs
+++ TechLekh.Core/Domain/Tag.cs
@@ -6,8 +6,6 @@
         public string Name { get; set; }
         public string DisplayName { get; set; }
 
-        public string Test { get; set; }
-
         public ICollection<BlogPost> BlogPosts { get; set; }
     }
 }
